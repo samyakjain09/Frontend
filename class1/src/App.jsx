a6fb78f5e5db5daf1af66e090753e1110a380559
samyakjain09 <samyakjain1p@gmail.com>
2026-03-13 21:59:45 +0530

    react folder structure

---
+++ class1/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from './components/Nav'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <div>
+      <h1><Footer/></h1>
+    </div>
+  )
+}
+
+export default App
