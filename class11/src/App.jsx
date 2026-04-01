0ecb470b7541cbf2382356fa8487c26903e60557
samyakjain09 <samyakjain1p@gmail.com>
2026-04-01 19:52:02 +0530

    react router dom

---
+++ class11/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from './pages/Home'
+import About from './pages/About'
+const App = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  )
+}
+
+export default App
---
+++ class11/src/pages/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div><h1 className='text-3xl underline font-bold absolute top-1/2 left-1/2 -translate-1/2'>About</h1></div>
+  )
+}
+
+export default About
