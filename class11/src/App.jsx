5882b3479062bf68df96f82dbab511b150572b0b
samyakjain09 <samyakjain1p@gmail.com>
2026-04-02 23:15:54 +0530

    react router dom 2

--- class11/src/App.jsx
+++ class11/src/App.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import Home from './pages/Home'
 import About from './pages/About'
+import { Link, Route,Routes } from 'react-router-dom'
+
 const App = () => {
   return (
     <div>
-      <Home />
+
+      <div className='flex justify-between px-8 py-4 bg-pink-900 mb-10'>
+        <h1>
+          Navbar
+        </h1>
+      </div>
+
+      <div>
+          <Link to='/'>Home</Link>
+          <Link to='/about'>about</Link>
+          <Link to='/store'>store</Link>
+          <Link to='/course'>course</Link>
+      </div>
+
+      <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about /id:' element={<About/>}/>
+    </Routes>
     </div>
+    
   )
 }
 
--- class11/src/pages/Home.jsx
+++ class11/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div><h1 className='text-3xl underline font-bold absolute top-1/2 left-1/2 -translate-1/2'>Home</h1></div>
+    <div><h1 className='text-3xl underline font-bold absolute top-10% left-1/2 -translate-1/2'>Home</h1></div>
   )
 }
 
