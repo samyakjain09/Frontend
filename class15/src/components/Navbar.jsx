62e0ec91868dba5409f818beb91d745ec0f00ee9
samyakjain09 <samyakjain1p@gmail.com>
2026-05-12 22:16:39 +0530

    day15

--- class15/src/components/Navbar.jsx
+++ class15/src/components/Navbar.jsx
@@ -7,3 +7,55 @@ const navbar = () => {
 }
 
 export default navbar
+
+
+
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>navbar</div>
+  )
+}
+
+export default navbarimport React from 'react'
+
+const navbar = () => {
+  return (
+    <div>navbar</div>
+  )
+}
+
+export default navbarimport React from 'react'
+
+const navbar = () => {
+  return (
+    <div>navbar</div>
+  )
+}
+
+export default navbarimport React from 'react'
+
+const navbar = () => {
+  return (
+    <div>navbar</div>
+  )
+}
+
+export default navbarimport React from 'react'
+
+const navbar = () => {
+  return (
+    <div>navbar</div>
+  )
+}
+
+export default navbarimport React from 'react'
+
+const navbar = () => {
+  return (
+    <div>navbar</div>
+  )
+}
+
+export default navbar
